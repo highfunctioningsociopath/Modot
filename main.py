a624b60ba7f2e415c51ce168445dd4066cfa45d5
Kavoya <epickavoya123@gmail.com>
2021-06-02 12:56:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ async def kick(ctx, member: discord.Member, *, reason=None):
 
 # Ban command
 @client.command(name="ban", aliases=["Ban", "BAN"])
-@commands.has_permissions(kick_members=True)
+@commands.has_permissions(ban_members=True)
 async def ban(ctx, member: discord.Member, *, reason=None):
     # Embed which will be sent when a person is banned
     embed = discord.Embed(colour=0xFF0000)
@@ -80,8 +80,39 @@ async def ban(ctx, member: discord.Member, *, reason=None):
     await ctx.send(embed=embed)  # Sends an embed with info in the
                                  # channel the command was used on
     await member.send(embed=embed2)  # DMs an embed with ban info
-                                     # to the person who was kicked
-    await member.kick(reason=reason)
+                                     # to the person who was banned
+    await member.ban(reason=reason)
+
+
+# Softban command
+@client.command(name="softban", aliases=["Softban", "SOFTBAN"])
+@commands.has_permissions(ban_members=True)
+async def softban(ctx, member: discord.Member, *, reason=None):
+    # Embed which will be sent when a person is softbanned
+    embed = discord.Embed(colour=0xFF0000)
+
+    embed.set_author(name=f'User Softanned | {member}', icon_url=member.avatar_url)
+
+    embed.add_field(name='User', value=f'{member.mention}', inline=True)
+
+    embed.add_field(name='Moderator',value=f'{ctx.author.mention}', inline=True)
+
+    embed.add_field(name='Reason', value=f'{reason}', inline=True)
+
+    # Embed which will be DMed to the person who was softbanned
+    embed2 = discord.Embed(description=f'You were softbanned from {ctx.guild.name}', colour=0xFF0000)
+
+    embed2.add_field(name='Reason', value=f'{reason}', inline=True)
+
+    embed2.add_field(name='Moderator', value=f'{ctx.author.name}', inline=True)
+
+    await ctx.send(embed=embed)  # Sends an embed with info in the
+                                 # channel the command was used on
+    await member.send(embed=embed2)  # DMs an embed with softban 
+                                     # info to the person who was softbanned
+    await member.ban(reason=reason)
+    await member.unban(reason=reason)
+
 
 '''
 You can do the following if you're using replit.com and this
