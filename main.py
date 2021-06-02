783961639dbc629af613354487d52f07798519f8
Kavoya <epickavoya123@gmail.com>
2021-06-02 12:46:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def on_ready():
 
 
 # Kick command
-@client.command()
+@client.command(name="kick", aliases=["Kick", "KICK"])
 @commands.has_permissions(kick_members=True)
 async def kick(ctx, member: discord.Member, *, reason=None):
     # Embed which will be sent when a person is kicked
@@ -37,10 +37,11 @@ async def kick(ctx, member: discord.Member, *, reason=None):
 
     embed.add_field(name='Reason', value=f'{reason}', inline=True)
     
-    # Embed which will be DMed to the person who was kicked out
-    embed2 = discord.Embed(description=f'You were banned from {ctx.guild.name}', colour=0xFF0000)
+    # Embed which will be DMed to the person who was kicked
+    embed2 = discord.Embed(description=f'You were kicked from {ctx.guild.name}', colour=0xFF0000)
     
     embed2.add_field(name='Reason', value=f'{reason}', inline=True)
+    
     embed2.add_field(name='Moderator', value=f'{ctx.author.name}', inline=True)
     
     
@@ -50,6 +51,37 @@ async def kick(ctx, member: discord.Member, *, reason=None):
                                      # to the person who was kicked
     await member.kick(reason=reason)
     
+
+# Ban command
+@client.command(name="ban", aliases=["Ban", "BAN"])
+@commands.has_permissions(kick_members=True)
+async def ban(ctx, member: discord.Member, *, reason=None):
+    # Embed which will be sent when a person is banned
+    embed = discord.Embed(colour=0xFF0000)
+
+    embed.set_author(
+        name=f'User Banned | {member}', icon_url=member.avatar_url)
+
+    embed.add_field(name='User', value=f'{member.mention}', inline=True)
+
+    embed.add_field(name='Moderator',
+                    value=f'{ctx.author.mention}', inline=True)
+
+    embed.add_field(name='Reason', value=f'{reason}', inline=True)
+
+    # Embed which will be DMed to the person who was banned
+    embed2 = discord.Embed(
+        description=f'You were banned from {ctx.guild.name}', colour=0xFF0000)
+
+    embed2.add_field(name='Reason', value=f'{reason}', inline=True)
+
+    embed2.add_field(name='Moderator', value=f'{ctx.author.name}', inline=True)
+
+    await ctx.send(embed=embed)  # Sends an embed with info in the
+                                 # channel the command was used on
+    await member.send(embed=embed2)  # DMs an embed with ban info
+                                     # to the person who was kicked
+    await member.kick(reason=reason)
 
 '''
 You can do the following if you're using replit.com and this
@@ -61,5 +93,6 @@ This way you don't need a '.env' file and no need to import os and dotenv
 Keep in mind that if someone can see your code, they will we able to see 
 your bot token and get access to your bot.
 '''
+
 dotenv.load_dotenv()
 client.run(os.environ.get("token"))
