90da0e5a596c00b1d8bbb7ed6bb5514c7ec38deb
Kavoya <78526936+Kavoyaa@users.noreply.github.com>
2021-06-02 18:02:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -133,16 +133,6 @@ async def help(ctx):
     
     await ctx.send(embed=embed)
 
-'''
-You can do the following if you're using replit.com and this
-method dosen't work:
-client.run(os.getenv('token'))
-You can also use the following if no one can see your code:
-client.run(<your bot token here>)
-This way you don't need a '.env' file and no need to import os and dotenv
-Keep in mind that if someone can see your code, they will we able to see 
-your bot token and get access to your bot.
-'''
-
+# Gets the environment variable "token" you made in .env file
 dotenv.load_dotenv()
 client.run(os.environ.get("token"))
